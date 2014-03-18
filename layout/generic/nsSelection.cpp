294b69b3915b399594c166167ad531977296e174
Andrew Halberstadt <ahalberstadt@mozilla.com>
2014-03-18 17:34:29 -0400

    Backout last two changesets

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -5644,7 +5644,6 @@ Selection::Modify(const nsAString& aAlter, const nsAString& aDirection,
   // The uint32_t casts below prevent an enum mismatch warning.
   nsSelectionAmount amount;
   uint32_t keycode;
-  amount = eSelectLine;
   if (aGranularity.LowerCaseEqualsLiteral("character")) {
     amount = eSelectCluster;
     keycode = forward ? (uint32_t) nsIDOMKeyEvent::DOM_VK_RIGHT :
